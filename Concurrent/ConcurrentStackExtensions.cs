4498d1a8bfe94031a74373456f4674da4c8f14da
Константин Дьяченко <konard@me.com>
2019-08-12 12:23:25 +0300

    Added PeekOrDefault extension method for ConcurrentStack<T>.

--- Concurrent/ConcurrentStackExtensions.cs
+++ Concurrent/ConcurrentStackExtensions.cs
@@ -7,5 +7,7 @@ namespace Platform.Collections.Concurrent
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T PopOrDefault<T>(this ConcurrentStack<T> stack) => stack.TryPop(out T value) ? value : default;
+
+        public static T PeekOrDefault<T>(this ConcurrentStack<T> stack) => stack.TryPeek(out T value) ? value : default;
     }
 }
