46959f24687465ec61c15e9672ecb93bce07a5f1
Константин Дьяченко <konard@me.com>
2019-08-12 12:05:21 +0300

    Closes #26.

---
+++ Stacks/IStackExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Platform.Collections.Stacks
+{
+    public static class IStackExtensions
+    {
+        public static void Clear<T>(this IStack<T> stack)
+        {
+            while (!stack.IsEmpty)
+            {
+                _ = stack.Pop();
+            }
+        }
+    }
+}
