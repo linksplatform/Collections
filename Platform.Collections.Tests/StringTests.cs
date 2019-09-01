9bd60f34181a827f0bc064275de9994e63ba257b
Константин Дьяченко <konard@me.com>
2019-09-01 13:14:19 +0300

    Closes #33.

--- Platform.Collections.Tests/StringTests.cs
+++ Platform.Collections.Tests/StringTests.cs
@@ -17,5 +17,25 @@ namespace Platform.Collections.Tests
             var result3 = source3.CapitalizeFirstLetter();
             Assert.Equal("  Hello", result3);
         }
+
+        [Fact]
+        public static void TrimSingleTest()
+        {
+            var source1 = "'";
+            var result1 = source1.TrimSingle('\'');
+            Assert.Equal("", result1);
+            var source2 = "''";
+            var result2 = source2.TrimSingle('\'');
+            Assert.Equal("", result2);
+            var source3 = "'hello'";
+            var result3 = source3.TrimSingle('\'');
+            Assert.Equal("hello", result3);
+            var source4 = "hello'";
+            var result4 = source4.TrimSingle('\'');
+            Assert.Equal("hello", result4);
+            var source5 = "'hello";
+            var result5 = source5.TrimSingle('\'');
+            Assert.Equal("hello", result5);
+        }
     }
 }
--- Platform.Collections/StringExtensions.cs
+++ Platform.Collections/StringExtensions.cs
@@ -7,19 +7,19 @@ namespace Platform.Collections
 {
     public static class StringExtensions
     {
-        public static string CapitalizeFirstLetter(this string str)
+        public static string CapitalizeFirstLetter(this string @string)
         {
-            if (string.IsNullOrWhiteSpace(str))
+            if (string.IsNullOrWhiteSpace(@string))
             {
-                return str;
+                return @string;
             }
-            var chars = str.ToCharArray();
+            var chars = @string.ToCharArray();
             for (var i = 0; i < chars.Length; i++)
             {
                 var category = char.GetUnicodeCategory(chars[i]);
                 if (category == UnicodeCategory.UppercaseLetter)
                 {
-                    return str;
+                    return @string;
                 }
                 if (category == UnicodeCategory.LowercaseLetter)
                 {
@@ -27,9 +27,45 @@ namespace Platform.Collections
                     return new string(chars);
                 }
             }
-            return str;
+            return @string;
         }
 
-        public static string Truncate(this string str, int maxLength) => string.IsNullOrEmpty(str) ? str : str.Substring(0, Math.Min(str.Length, maxLength));
+        public static string Truncate(this string @string, int maxLength) => string.IsNullOrEmpty(@string) ? @string : @string.Substring(0, Math.Min(@string.Length, maxLength));
+
+        public static string TrimSingle(this string @string, char charToTrim)
+        {
+            if (!string.IsNullOrEmpty(@string))
+            {
+                if (@string.Length == 1)
+                {
+                    if (@string[0] == charToTrim)
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return @string;
+                    }
+                }
+                else
+                {
+                    var left = 0;
+                    var right = @string.Length - 1;
+                    if (@string[left] == charToTrim)
+                    {
+                        left++;
+                    }
+                    if (@string[right] == charToTrim)
+                    {
+                        right--;
+                    }
+                    return @string.Substring(left, right - left + 1);
+                }
+            }
+            else
+            {
+                return @string;
+            }
+        }
     }
 }
