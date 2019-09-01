83932f9c30b61b02a350b27da3392c3c8e3adccb
Константин Дьяченко <konard@me.com>
2019-09-01 13:17:30 +0300

    Closes #32.

---
+++ Platform.Collections/Arrays/GenericArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Collections.Arrays
+{
+    public static class GenericArrayExtensions
+    {
+        public static T[] Clone<T>(this T[] array)
+        {
+            var copy = new T[array.Length];
+            Array.Copy(array, 0, copy, 0, array.Length);
+            return copy;
+        }
+    }
+}
