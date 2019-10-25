5ffe121c86d3daf57cab764fd2fdce52284f511f
Константин Дьяченко <konard@me.com>
2019-10-25 23:32:06 +0300

    Closes #47.

---
+++ Platform.Collections.Benchmarks/BitStringBenchmarks.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Platform.Collections.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    [WarmupCount(2)]
+    [IterationCount(1)]
+    public class BitStringBenchmarks
+    {
+        [Params(1000, 10000, 100000)]
+        public int N;
+        private BitString _left;
+        private BitString _right;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _left = new BitString(N);
+            _right = new BitString(N);
+            while (_left.Equals(_right))
+            {
+                _left.SetRandomBits();
+                _right.SetRandomBits();
+            }
+        }
+
+        [Benchmark]
+        public BitString Not() => new BitString(_left).Not();
+
+        [Benchmark]
+        public BitString VectorNot() => new BitString(_left).VectorNot();
+
+        [Benchmark]
+        public BitString ParallelNot() => new BitString(_left).ParallelNot();
+
+        [Benchmark]
+        public BitString ParallelVectorNot() => new BitString(_left).ParallelVectorNot();
+
+        [Benchmark]
+        public BitString And() => new BitString(_left).And(_right);
+
+        [Benchmark]
+        public BitString VectorAnd() => new BitString(_left).VectorAnd(_right);
+
+        [Benchmark]
+        public BitString ParallelAnd() => new BitString(_left).ParallelAnd(_right);
+
+        [Benchmark]
+        public BitString ParallelVectorAnd() => new BitString(_left).ParallelVectorAnd(_right);
+
+        [Benchmark]
+        public BitString Or() => new BitString(_left).Or(_right);
+
+        [Benchmark]
+        public BitString VectorOr() => new BitString(_left).VectorOr(_right);
+
+        [Benchmark]
+        public BitString ParallelOr() => new BitString(_left).ParallelOr(_right);
+
+        [Benchmark]
+        public BitString ParallelVectorOr() => new BitString(_left).ParallelVectorOr(_right);
+
+        [Benchmark]
+        public BitString Xor() => new BitString(_left).Xor(_right);
+
+        [Benchmark]
+        public BitString VectorXor() => new BitString(_left).VectorXor(_right);
+
+        [Benchmark]
+        public BitString ParallelXor() => new BitString(_left).ParallelXor(_right);
+
+        [Benchmark]
+        public BitString ParallelVectorXor() => new BitString(_left).ParallelVectorXor(_right);
+    }
+}
---
+++ Platform.Collections.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Platform.Collections.Benchmarks
+{
+    class Program
+    {
+        static void Main() => BenchmarkRunner.Run<BitStringBenchmarks>();
+    }
+}
