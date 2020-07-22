ef81e5c2de3b0548cf08bb65dc0f3dc9e99d16d2
Tynkute <aktivnygomes1@gmail.com>
2020-07-22 17:05:24 +0300

    Added comments

--- csharp/Platform.Collections.Benchmarks/Program.cs
+++ csharp/Platform.Collections.Benchmarks/Program.cs
@@ -4,6 +4,10 @@ namespace Platform.Collections.Benchmarks
 {
     static class Program
     {
+        /// <summary>
+        /// <para>Initializing the Benchmark test.</para>
+        /// <para>Инициализация Бенчмарк теста.</para>
+        /// </summary>
         static void Main() => BenchmarkRunner.Run<BitStringBenchmarks>();
     }
 }
