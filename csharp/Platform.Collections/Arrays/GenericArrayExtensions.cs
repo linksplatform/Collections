d31aea12daadfa5b831ed898bc4232ef950c5af5
Tynkute <aktivnygomes1@gmail.com>
2020-07-23 12:03:21 +0300

    Comment

--- csharp/Platform.Collections/Arrays/GenericArrayExtensions.cs
+++ csharp/Platform.Collections/Arrays/GenericArrayExtensions.cs
@@ -56,7 +56,17 @@ namespace Platform.Collections.Arrays
                 return false;
             }
         }
-
+        
+        /// <summary>
+        /// <param name="array"><para>Array that will participate in verification.</para><para>Массив который будет учавствовать в проверке.</para></param>
+        /// <param name="index"><para>Number type long to compare.</para><para>Число типа long для сравнения.</para></param>
+        /// <param name="element"><para>Passing the argument by reference, if successful, it will take the value array[index] otherwise default value.</para><para>Передаём аргумент по ссылке, в случае успеха он примет значение array[index] в противном случае значение по умолчанию.</para></param>
+        /// <para>We check whether the array exist, if so, we check the array length using the index varible type long, and if the array length is greater than the index, we set the element variable to array[index] and return true.</para>
+        /// <para>Мы проверяем, существует ли массив, если да, то мы проверяем длину массива с помощью переменной index  типа long, и если длина массива больше значения index, мы устанавливаем значение переменной element - array[index] и возвращаем true.</para>
+        /// </summary>
+        /// <typeparam name="T"><para>Array variable type.</para><para>Тип переменной массива.</para></typeparam>
+        /// <returns><para>True if successful otherwise false.</para><para>True в случае успеха, в противном случае false</para></returns>
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool TryGetElement<T>(this T[] array, long index, out T element)
         {
