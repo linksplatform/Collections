e5c5a36db89d9208e8951228fc60ac1e93b4bea6
Tynkute <aktivnygomes1@gmail.com>
2020-09-21 15:38:24 +0300

    Bug fixed

--- csharp/Platform.Collections/Lists/IListEqualityComparer.cs
+++ csharp/Platform.Collections/Lists/IListEqualityComparer.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS1591 
 
 namespace Platform.Collections.Lists
 {
--- csharp/Platform.Collections/Lists/IListExtensions.cs
+++ csharp/Platform.Collections/Lists/IListExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS1591 
 
 namespace Platform.Collections.Lists
 {
