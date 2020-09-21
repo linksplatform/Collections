434f64c486eaec74259511837abc6d087e375c29
Tynkute <aktivnygomes1@gmail.com>
2020-09-21 15:46:07 +0300

    bug fixed?

--- csharp/Platform.Collections/Lists/CharIListExtensions.cs
+++ csharp/Platform.Collections/Lists/CharIListExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS1591 
 
 namespace Platform.Collections.Lists
 {
@@ -11,7 +11,7 @@ namespace Platform.Collections.Lists
         /// <para>Generates a hash code for the entire list based on the values of its elements.</para>
         /// <para>Генерирует хэш-код всего списка, на основе значений его элементов.</para>
         /// </summary>
-        /// <param name="list"><para>Hash list.</para><para>Список для хеширования.</para></param>
+        /// <param name="list"><para>The list to be hashed.</para><para>Список для хеширования.</para></param>
         /// <returns>
         /// <para>The hash code of the list.</para>
         /// <para>Хэш-код списка.</para>
@@ -38,15 +38,15 @@ namespace Platform.Collections.Lists
         /// <param name="left"><para>The first compared list.</para><para>Первый список для сравнения.</para></param>
         /// <param name="right"><para>The second compared list.</para><para>Второй список для сравнения.</para></param>
         /// <returns>
-        /// <para>If the passed lists are equal to each other, true is returned, оtherwise false.</para>
-        /// <para>Если переданные списки равны друг другу, возвращается true, иначе же false.</para>
+        /// <para>True, if the passed lists are equal to each other оtherwise false.</para>
+        /// <para>True, если переданные списки равны друг другу, иначе false.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool EqualTo(this IList<char> left, IList<char> right) => left.EqualTo(right, ContentEqualTo);
 
         /// <summary>
-        /// <para>Compares each element in the list for identity.</para>
-        /// <para>Сравнивает на идентичность каждый элемент списка.</para>
+        /// <para>Compares each element in the list for equality.</para>
+        /// <para>Сравнивает на равенство каждый элемент списка.</para>
         /// </summary>
         /// <param name="left"><para>The first compared list.</para><para>Первый список для сравнения.</para></param>
         /// <param name="right"><para>The second compared list.</para><para>Второй список для сравнения.</para></param>
