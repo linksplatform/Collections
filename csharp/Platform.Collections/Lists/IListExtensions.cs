1da6796ed9a1de26d7eaa166d41f45d39d3e771c
Tynkute <aktivnygomes1@gmail.com>
2020-09-21 17:00:04 +0300

    Pragma once deleted

--- csharp/Platform.Collections/Lists/IListExtensions.cs
+++ csharp/Platform.Collections/Lists/IListExtensions.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 
-
 namespace Platform.Collections.Lists
 {
     public static class IListExtensions
