39b6065b46aa5ff9266e55d76788c852f30b5022
Tynkute <aktivnygomes1@gmail.com>
2020-11-07 20:12:38 +0300

    Oops, bug ^^

--- csharp/Platform.Collections/Lists/IListExtensions.cs
+++ csharp/Platform.Collections/Lists/IListExtensions.cs
@@ -233,7 +233,7 @@ namespace Platform.Collections.Lists
 
         /// <summary>
         /// <para>Compares each element in the list for identity.</para>
-        /// <para>Сравнивает на идентичность каждый элемент списка.</para>
+        /// <para>Сравнивает на равенство каждый элемент списка.</para>
         /// </summary>
         /// <typeparam name="T"><para>The list's item type.</para><para>Тип элементов списка.</para></typeparam>
         /// <param name="left"><para>The first compared list.</para><para>Первый список для сравнения.</para></param>
@@ -371,7 +371,7 @@ namespace Platform.Collections.Lists
         ///             <description>First non equal element of <paramref name="left" /> list is greater than first not equal element of <paramref name="right" /> list.</description>
         ///         </item>
         ///     </list>
-        /// <para>
+        /// </para>
         /// <para>
         ///     Целое число со знаком, которое указывает относительные значения элементов списков <paramref name="left" /> и <paramref name="right" /> как показано в следующей таблице.
         ///     <list type="table">
