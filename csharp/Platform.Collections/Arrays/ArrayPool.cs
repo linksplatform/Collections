fba9a65586fe11f1a4b007e9fc61fc2e53dafeb9
Tynkute <aktivnygomes1@gmail.com>
2020-11-21 18:14:20 +0300

    Added comments

--- csharp/Platform.Collections/Arrays/ArrayPool.cs
+++ csharp/Platform.Collections/Arrays/ArrayPool.cs
@@ -1,7 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 namespace Platform.Collections.Arrays
 {
     public static class ArrayPool
@@ -9,9 +7,25 @@ namespace Platform.Collections.Arrays
         public static readonly int DefaultSizesAmount = 512;
         public static readonly int DefaultMaxArraysPerSize = 32;
 
+        /// <summary>
+        /// <para>Allocating a block of a certain size from a memory area.</para>
+        /// <para>Выделение блока определенного размера из области памяти.</para>
+        /// </summary>
+        /// <typeparam name="T"><para>Type to allocate array for.</para><para>Тип для аллокации массива.</para></typeparam>
+        /// <param name="size"><para>The memory block size.</para><para>Размер блока памяти.</para></param>
+        /// <returns>
+        /// <para>The memory allocated for the array.</para>
+        /// <para>Память выделенная для массива.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T[] Allocate<T>(long size) => ArrayPool<T>.ThreadInstance.Allocate(size);
 
+        /// <summary>
+        /// <para>Freeing a block of allocated heap.</para>
+        /// <para>Освобождение блока выделенной памяти.</para>
+        /// </summary>
+        /// <typeparam name="T"><para>Type to allocate array for.</para><para>Тип для аллокации массива.</para></typeparam>
+        /// <param name="array"><para>The memory allocated for the array.</para><para>Память выделенная для массива.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Free<T>(T[] array) => ArrayPool<T>.ThreadInstance.Free(array);
     }
