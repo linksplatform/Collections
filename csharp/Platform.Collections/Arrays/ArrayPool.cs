9fdd6752654752a7741c9387b70fe88b4239a4ab
Tynkute <aktivnygomes1@gmail.com>
2020-11-22 15:01:05 +0300

    fix syntax errors

--- csharp/Platform.Collections/Arrays/ArrayPool.cs
+++ csharp/Platform.Collections/Arrays/ArrayPool.cs
@@ -9,23 +9,23 @@ namespace Platform.Collections.Arrays
 
         /// <summary>
         /// <para>Allocation of an array of a certain size from the array pool.</para>
-        /// <para>Выделение массива определённого размера из пула массивов.</para>
+        /// <para>Выделение массива указанного размера из пула массивов.</para>
         /// </summary>
-        /// <typeparam name="T"><para>Array elements type.</para><para>Тип элементов массива.</para></typeparam>
+        /// <typeparam name="T"><para>The array elements type.</para><para>Тип элементов массива.</para></typeparam>
         /// <param name="size"><para>Allocated array size.</para><para>Размер выделяемого массива.</para></param>
         /// <returns>
-        /// <para>Array from a pool of arrays.</para>
+        /// <para>The array from a pool of arrays.</para>
         /// <para>Массив из пулла массивов.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T[] Allocate<T>(long size) => ArrayPool<T>.ThreadInstance.Allocate(size);
 
         /// <summary>
-        /// <para>Freeing an array from an array pool.</para>
-        /// <para>Освобождение массива в пула массивов.</para>
+        /// <para>Freeing an array into an array pool.</para>
+        /// <para>Освобождение массива в пул массивов.</para>
         /// </summary>
-        /// <typeparam name="T"><para>Array elements type.</para><para>Тип элементов массива.</para></typeparam>
-        /// <param name="array"><para>The array to be freed from in pool.</para><para>Массив который нужно освобоить в пулл.</para></param>
+        /// <typeparam name="T"><para>The array elements type.</para><para>Тип элементов массива.</para></typeparam>
+        /// <param name="array"><para>The array to be freed into the pull.</para><para>Массив который нужно освобоить в пулл.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Free<T>(T[] array) => ArrayPool<T>.ThreadInstance.Free(array);
     }
