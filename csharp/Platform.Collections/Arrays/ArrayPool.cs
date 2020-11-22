21a138d36099526da63438bef02815f008381bfa
Tynkute <aktivnygomes1@gmail.com>
2020-11-22 14:52:31 +0300

    Yes.

--- csharp/Platform.Collections/Arrays/ArrayPool.cs
+++ csharp/Platform.Collections/Arrays/ArrayPool.cs
@@ -14,18 +14,18 @@ namespace Platform.Collections.Arrays
         /// <typeparam name="T"><para>Array elements type.</para><para>Тип элементов массива.</para></typeparam>
         /// <param name="size"><para>Allocated array size.</para><para>Размер выделяемого массива.</para></param>
         /// <returns>
-        /// <para>The memory allocated for the array.</para>
-        /// <para>Память выделенная для массива.</para>
+        /// <para>Array from a pool of arrays.</para>
+        /// <para>Массив из пулла массивов.</para>
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T[] Allocate<T>(long size) => ArrayPool<T>.ThreadInstance.Allocate(size);
 
         /// <summary>
         /// <para>Freeing an array from an array pool.</para>
-        /// <para>Освобождение массива из пула массивов.</para>
+        /// <para>Освобождение массива в пула массивов.</para>
         /// </summary>
         /// <typeparam name="T"><para>Array elements type.</para><para>Тип элементов массива.</para></typeparam>
-        /// <param name="array"><para>The array to be freed from the pool.</para><para>Массив который нужно освобоить из пулла.</para></param>
+        /// <param name="array"><para>The array to be freed from in pool.</para><para>Массив который нужно освобоить в пулл.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Free<T>(T[] array) => ArrayPool<T>.ThreadInstance.Free(array);
     }
