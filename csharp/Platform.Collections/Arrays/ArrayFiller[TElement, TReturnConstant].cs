b1b6e612d0200a4b630c6911ab8daccee554d499
213 <aktivnygomes1@gmail.com>
2021-01-24 20:03:17 +0300

    comments (#105)
    * Added some comments

--- csharp/Platform.Collections/Arrays/ArrayFiller[TElement, TReturnConstant].cs
+++ csharp/Platform.Collections/Arrays/ArrayFiller[TElement, TReturnConstant].cs
@@ -1,29 +1,82 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 namespace Platform.Collections.Arrays
 {
+    /// <summary>
+    /// <para>Represents <see cref="T:TElement[]"/> array filler with additional methods that return a given constant of type <typeparamref cref="TReturnConstant"/>.</para>
+    /// <para>Представляет заполнитель массива <see cref="T:TElement[]"/> c дополнительными методами, возвращающими заданную константу типа <typeparamref cref="TReturnConstant"/>.</para> 
+    /// </summary>
+    /// <typeparam name="TElement"><para>The elements' type.</para><para>Тип элементов массива.</para></typeparam>
+    /// <typeparam name="TReturnConstant"><para>The return constant's type.</para><para>Тип возвращаемой константы.</para></typeparam>
     public class ArrayFiller<TElement, TReturnConstant> : ArrayFiller<TElement>
     {
         protected readonly TReturnConstant _returnConstant;
 
+        /// <summary>
+        /// <para>Initializes a new instance of the <see cref="ArrayFiller"/> class using the specified array, the offset from which filling will start and the constant returned when elements are being filled.</para>
+        /// <para>Инициализирует новый экземпляр класса <see cref="ArrayFiller"/>, используя указанный массив, смещение с которого начнётся заполнение и константу возвращаемую при заполнении элементов.</para>
+        /// </summary>
+        /// <param name="array"><para>The array to fill.</para><para>Массив для заполнения.</para></param>
+        /// <param name="offset"><para>The offset from which to start the array filling.</para><para>Смещение с которого начнётся заполнение массива.</para></param>
+        /// <param name="returnConstant"><para>The constant's value.</para><para>Значение константы.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayFiller(TElement[] array, long offset, TReturnConstant returnConstant) : base(array, offset) => _returnConstant = returnConstant;
 
+        /// <summary>
+        /// <para>Initializes a new instance of the <see cref="ArrayFiller"/> class using the specified array and the constant returned when elements are being filled. Filling will start from the beginning of the array.</para>
+        /// <para>Инициализирует новый экземпляр класса <see cref="ArrayFiller"/>, используя указанный массив и константу возвращаемую при заполнении элементов. Заполнение начнётся с начала массива.</para>
+        /// </summary>
+        /// <param name="array"><para>The array to fill.</para><para>Массив для заполнения.</para></param>
+        /// <param name="returnConstant"><para>The constant's value.</para><para>Значение константы.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayFiller(TElement[] array, TReturnConstant returnConstant) : this(array, 0, returnConstant) { }
 
+        /// <summary>
+        /// <para>Adds an item into the array and returns the constant.</para>
+        /// <para>Добавляет элемент в массив и возвращает константу.</para>
+        /// </summary>
+        /// <param name="element"><para>The element to add.</para><para>Добавляемый элемент.</para></param>
+        /// <returns>
+        /// <para>The constant's value.</para>
+        /// <para>Значение константы.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TReturnConstant AddAndReturnConstant(TElement element) => _array.AddAndReturnConstant(ref _position, element, _returnConstant);
 
+        /// <summary>
+        /// <para>Adds the first element from the specified list to the filled array and returns the constant.</para>
+        /// <para>Добавляет первый элемент из указанного списка в заполняемый массив и возвращает константу.</para>
+        /// </summary>
+        /// <param name="element"><para>The list from which the first item will be added.</para><para>Список из которого будет добавлен первый элемент.</para></param>
+        /// <returns>
+        /// <para>The constant's value.</para>
+        /// <para>Значение константы.</para>
+        /// <returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TReturnConstant AddFirstAndReturnConstant(IList<TElement> elements) => _array.AddFirstAndReturnConstant(ref _position, elements, _returnConstant);
 
+        /// <summary>
+        /// <para>Adds all elements from the specified list to the filled array and returns the constant.</para>
+        /// <para>Добавляет все элементы из указанного списка в заполняемый массив и возвращает константу.</para>
+        /// </summary>
+        /// <param name="elements"><para>The list of values to add.</para><para>Список значений для добавления.</para></param>
+        /// <returns>
+        /// <para>The constant's value.</para>
+        /// <para>Значение константы.</para>
+        /// <returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TReturnConstant AddAllAndReturnConstant(IList<TElement> elements) => _array.AddAllAndReturnConstant(ref _position, elements, _returnConstant);
 
+        /// <summary>
+        /// <para>Adds the elements of the list to the array, skipping the first element and returns the constant.</para>
+        /// <para>Добавляет элементы списка в массив пропуская первый элемент и возвращает константу.</para>
+        /// </summary>
+        /// <param name="elements"><para>The list of values to add.</para><para>Список значений для добавления.</para></param>
+        /// <returns>
+        /// <para>The constant's value.</para>
+        /// <para>Значение константы.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TReturnConstant AddSkipFirstAndReturnConstant(IList<TElement> elements) => _array.AddSkipFirstAndReturnConstant(ref _position, elements, _returnConstant);
     }
--- csharp/Platform.Collections/Arrays/ArrayFiller[TElement].cs
+++ csharp/Platform.Collections/Arrays/ArrayFiller[TElement].cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 namespace Platform.Collections.Arrays
 {
+    /// <summary>
+    /// <para>Represents an <see cref="T:TElement[]"/> array filler.</para>
+    /// <para>Представляет заполнитель массива <see cref="T:TElement[]"/>.</para>
+    /// </summary>
+    /// <typeparam name="TElement"><para>The elements' type.</para><para>Тип элементов массива.</para></typeparam>
     public class ArrayFiller<TElement>
     {
         protected readonly TElement[] _array;
         protected long _position;
 
+        /// <summary>
+        /// <para>Initializes a new instance of the <see cref="ArrayFiller"/> class using the specified array as the array to fill and the offset from which to start filling.</para></para>
+        /// <para>Инициализирует новый экземпляр класса <see cref="ArrayFiller"/>, используя указанный массив в качестве заполняемого и смещение с которого начнётся заполнение.</para>
+        /// </summary>
+        /// <param name="array"><para>The array to fill.</para><para>Массив для заполнения.</para></param>
+        /// <param name="offset"><para>The offset from which to start filling the array.</para><para>Смещение с которого начнётся заполнение массива.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayFiller(TElement[] array, long offset)
         {
@@ -17,21 +26,67 @@ namespace Platform.Collections.Arrays
             _position = offset;
         }
 
+        /// <summary>
+        /// <para>Initializes a new instance of the <see cref="ArrayFiller"/> class using the specified array. Filling will start from the beginning of the array.</para>
+        /// <para>Инициализирует новый экземпляр класса <see cref="ArrayFiller"/>, используя указанный массив. Заполнение начнётся с начала массива.</para>
+        /// </summary>
+        /// <param name="array"><para>The array to fill.</para><para>Массив для заполнения.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayFiller(TElement[] array) : this(array, 0) { }
 
+        /// <summary>
+        /// <para>Adds an item into the array.</para>
+        /// <para>Добавляет элемент в массив.</para>
+        /// </summary>
+        /// <param name="element"><para>The element to add.</para><para>Добавляемый элемент.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Add(TElement element) => _array[_position++] = element;
 
+        /// <summary>
+        /// <para>Adds an item into the array and returns <see langword="true"/>.</para>
+        /// <para>Добавляет элемент в массив и возвращает <see langword="true"/>.</para>
+        /// </summary>
+        /// <param name="element"><para>The element to add.</para><para>Добавляемый элемент.</para></param>
+        /// <returns>
+        /// <para>The <see langword="true"/> value.</para>
+        /// <para>Значение <see langword="true"/>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool AddAndReturnTrue(TElement element) => _array.AddAndReturnConstant(ref _position, element, true);
 
+        /// <summary>
+        /// <para>Adds the first element from the specified list to the array to fill and returns <see langword="true"/>.</para>
+        /// <para>Добавляет первый элемент из указанного списка в заполняемый массив и возвращает <see langword="true"/>.</para>
+        /// </summary>
+        /// <param name="element"><para>The list from which the first item will be added.</para><para>Список из которого будет добавлен первый элемент.</para></param>
+        /// <returns>
+        /// <para>The <see langword="true"/> value.</para>
+        /// <para>Значение <see langword="true"/>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool AddFirstAndReturnTrue(IList<TElement> elements) => _array.AddFirstAndReturnConstant(ref _position, elements, true);
-
+        
+        /// <summary>
+        /// <para>Adds all elements from the specified list to the array to fill and returns <see langword="true"/>.</para>
+        /// <para>Добавляет все элементы из указанного списка в заполняемый массив и возвращает <see langword="true"/>.</para>
+        /// </summary>
+        /// <param name="elements"><para>The list of values to add.</para><para>Список значений которые необходимо добавить.</para></param>
+        /// <returns>
+        /// <para>The <see langword="true"/> value.</para>
+        /// <para>Значение <see langword="true"/>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool AddAllAndReturnTrue(IList<TElement> elements) => _array.AddAllAndReturnConstant(ref _position, elements, true);
 
+        /// <summary>
+        /// <para>Adds values to the array skipping the first element and returns <see langword="true"/>.</para>
+        /// <para>Добавляет значения в массив пропуская первый элемент и возвращает <see langword="true"/>.</para>
+        /// </summary>
+        /// <param name="elements"><para>A list from which elements will be added except the first.</para><para>Список из которого будут добавлены элементы кроме первого.</para></param>
+        /// <returns>
+        /// <para>The <see langword="true"/> value.</para>
+        /// <para>Значение <see langword="true"/>.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool AddSkipFirstAndReturnTrue(IList<TElement> elements) => _array.AddSkipFirstAndReturnConstant(ref _position, elements, true);
     }
