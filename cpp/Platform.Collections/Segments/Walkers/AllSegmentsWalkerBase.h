a2229a7e0487d44eb55c3902d02a1fc1b5e089c5
002_cap <dodickgod@gmail.com>
2021-03-28 13:02:48 +0300

    Segment walkers

--- cpp/Platform.Collections/Segments/Walkers/AllSegmentsWalkerBase.h
+++ cpp/Platform.Collections/Segments/Walkers/AllSegmentsWalkerBase.h
@@ -1,6 +1,7 @@
 ﻿namespace Platform::Collections::Segments::Walkers
 {
-    class AllSegmentsWalkerBase
+    template <typename ...> class AllSegmentsWalkerBase;
+    template<> class AllSegmentsWalkerBase<>
     {
         public: inline static const std::int32_t DefaultMinimumStringSegmentLength = 2;
     };
