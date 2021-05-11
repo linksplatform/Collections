b56401688b444ba14722385534d475946c3edba6
uselessgoddess <dodickgod@gmail.com>
2021-05-11 09:13:27 +0300

    Removed UB))

--- cpp/Platform.Collections/Segments/CharSegment.h
+++ cpp/Platform.Collections/Segments/CharSegment.h
@@ -1,17 +1,2 @@
 ﻿// Далее будет использоваться std::span<char>
 // Next, std::span<char> will be used
-
-namespace std
-{
-    template<typename TChar>
-    requires
-        std::same_as<TChar, char> ||
-        std::same_as<TChar, wchar_t> ||
-        std::same_as<TChar, char8_t> ||
-        std::same_as<TChar, char16_t> ||
-        std::same_as<TChar, char32_t>
-    std::string to_string(std::span<TChar> segment)
-    {
-        return std::string(segment.begin(), segment.end());
-    }
-}
