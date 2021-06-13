9fdbd272869ca4debb5da301e9e519ff1ccd51e9
uselessgoddess <dodickgod@gmail.com>
2021-06-13 21:38:47 +0300

    Namespace StringExtensions removed

--- cpp/Platform.Collections.Tests/StringTests.cpp
+++ cpp/Platform.Collections.Tests/StringTests.cpp
@@ -4,17 +4,17 @@ namespace Platform::Collections::Tests
 {
     TEST(StringTests, CapitalizeFirstLetterTest)
     {
-        ASSERT_EQ("Hello", StringExtensions::CapitalizeFirstLetter(std::string{"hello"}));
-        ASSERT_EQ("Hello", StringExtensions::CapitalizeFirstLetter(std::string{"Hello"}));
-        ASSERT_EQ("  Hello", StringExtensions::CapitalizeFirstLetter(std::string{"  hello"}));
+        ASSERT_EQ("Hello", CapitalizeFirstLetter(std::string{"hello"}));
+        ASSERT_EQ("Hello", CapitalizeFirstLetter(std::string{"Hello"}));
+        ASSERT_EQ("  Hello", CapitalizeFirstLetter(std::string{"  hello"}));
     }
 
     TEST(StringTests, TrimSingleTest)
     {
-        ASSERT_EQ("", StringExtensions::TrimSingle(std::string{"'"}, '\''));
-        ASSERT_EQ("", StringExtensions::TrimSingle(std::string{"''"}, '\''));
-        ASSERT_EQ("hello", StringExtensions::TrimSingle(std::string{"'hello'"}, '\''));
-        ASSERT_EQ("hello", StringExtensions::TrimSingle(std::string{"hello'"}, '\''));
-        ASSERT_EQ("hello", StringExtensions::TrimSingle(std::string{"'hello"}, '\''));
+        ASSERT_EQ("", TrimSingle(std::string{"'"}, '\''));
+        ASSERT_EQ("", TrimSingle(std::string{"''"}, '\''));
+        ASSERT_EQ("hello", TrimSingle(std::string{"'hello'"}, '\''));
+        ASSERT_EQ("hello", TrimSingle(std::string{"hello'"}, '\''));
+        ASSERT_EQ("hello", TrimSingle(std::string{"'hello"}, '\''));
     }
 }// namespace Platform::Collections::Tests
