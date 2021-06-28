bacd040802388e540bb1bf23964d2929745e5fb6
Binatik <sadxas999@mail.ru>
2021-06-28 20:49:17 +0300

    comment on GetOrDefault

--- csharp/Platform.Collections/IDictionaryExtensions.cs
+++ csharp/Platform.Collections/IDictionaryExtensions.cs
@@ -4,10 +4,26 @@ using System.Runtime.CompilerServices;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace Platform.Collections //Пространства имен
+namespace Platform.Collections 
 {
+    /// <summary>
+    /// <para>Represents a set of extension methods<see cref="T:T[]"/> array.</para>
+    /// <para>Представляет набор методов расширения<see cref="T:T[]"/>.</para>
+    /// </summary>
     public static class IDictionaryExtensions
     {
+        /// <typeparam name="dictionary"> 
+        /// <para>Dictionary that is a key pair:meaning.</para> 
+        /// <para>Словарь который пара ключ:значение</para> 
+        /// </typeparam>  
+        /// <typeparam name="key"> 
+        /// <para>Key.</para> 
+        /// <para>Ключ.</para> 
+        /// </typeparam> 
+        /// <returns> 
+        /// <para>The value of the active dictionary.</para> 
+        /// <para>Значение активного словаря.</para> 
+        /// </returns>        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
