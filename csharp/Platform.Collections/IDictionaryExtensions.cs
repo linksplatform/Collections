80388754b9db63f58c7afefb367c4edd9bd4a4fc
Binatik <sadxas999@mail.ru>
2021-07-10 18:36:43 +0300

    fix comments:

--- csharp/Platform.Collections/IDictionaryExtensions.cs
+++ csharp/Platform.Collections/IDictionaryExtensions.cs
@@ -13,20 +13,37 @@ namespace Platform.Collections
     public static class IDictionaryExtensions
     {
         /// <summary>    
-        /// <para>We get the default value.</para> 
-        /// <para>Получаем дефолтное значение.</para> 
-        /// </summary>  
+        /// <para> 
+        /// Returns the value associated with the passed parameters  
+        /// <paramref name="key"/> in <paramref name="dictionary"/> 
+        /// </para>  
+        /// <para> 
+        /// Возвращает значение связанное с переданными параметрами  
+        /// <paramref name="key"/> в <paramref name="dictionary"/> 
+        /// </para> 
+        /// </summary>   
+        /// 
         /// <typeparam name="dictionary"> 
-        /// <para>Dictionary that is a key pair:meaning.</para> 
-        /// <para>Словарь который пара ключ:значение</para> 
-        /// </typeparam>  
+        /// <para>  
+        /// <see cref="IDictionary<TKey, TValue>"/> from which to find the value associated with the <paramref name="key"/>.
+        /// </para> 
+        /// <see cref="IDictionary<TKey, TValue>"/> из которого нужно найти значение, связанное с <paramref name="key"/>.
+        /// </typeparam>   
+        /// 
         /// <typeparam name="key"> 
-        /// <para>Key.</para> 
+        /// <para>The key.</para> 
         /// <para>Ключ.</para> 
-        /// </typeparam> 
+        /// </typeparam>  
+        /// 
         /// <returns> 
-        /// <para>The value of the active dictionary.</para> 
-        /// <para>Значение активного словаря.</para> 
+        /// <para> 
+        /// The value associated with the   
+        /// <paramref name="key"/> in the <paramref name="dictionary"/> 
+        /// </para>  
+        /// <para> 
+        /// Значение, связанное с
+        /// <paramref name="key" в <paramref name="dictionary"/>
+        /// </para> 
         /// </returns>        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
@@ -36,24 +53,41 @@ namespace Platform.Collections
         }
 
         /// <summary>    
-        /// <para>Adding a value.</para> 
-        /// <para>Добавляем значение.</para> 
+        /// <para>Gets the value associated with the key in the dictionary or adds it if it does not exist.</para> 
+        /// <para>Получает значение, связанное с ключом в словаре, или добавляет его, если он не существует.</para> 
         /// </summary>   
+        /// 
         /// <typeparam name="dictionary"> 
-        /// <para>Dictionary that is a key pair:meaning.</para> 
-        /// <para>Словарь который пара ключ:значение</para> 
-        /// </typeparam>  
+        /// <para>  
+        /// An <see cref="IDictionary<TKey, TValue>"/> to find the value associated with the  
+        /// <paramref name="key"/> or to which to add a key-value pair if the  
+        /// <paramref name="key"/> does not exist.
+        /// </para> 
+        /// <para>  
+        /// Экземпляр <see cref="IDictionary<TKey, TValue>"/> из которого нужно найти значение ключа 
+        /// <paramref name="key"/>, или добавить его, если ключ не существует.
+        /// </para> 
+        /// </typeparam>   
+        /// 
         /// <typeparam name="key"> 
-        /// <para>Key.</para> 
-        /// <para>Ключ.</para> 
+        /// <para>The key of the element to add.</para> 
+        /// <para>Ключ добавляемого элемента.</para> 
         /// </typeparam>  
+        ///  
         /// <typeparam name="valueFactory">  
-        /// <para>The function, will return the value of the argument, which is the key.</para> 
-        /// <para>Функция, вернет значение по аргументу, который ключ.</para> 
-        /// </typeparam> 
+        /// <para>The function that gets the key and returns a value for it.</para> 
+        /// <para>Функция, которая получает ключ и возвращает значение для него.</para> 
+        /// </typeparam>  
+        /// 
         /// <returns> 
-        /// <para>Value that is not in the dictionary.</para> 
-        /// <para>Значение, которого нет в dictionary.</para> 
+        /// <para>The value associated with the 
+        /// <paramref name="key"/> in the  
+        /// <paramref name="dictionary"/> if it exists; otherwise new value. 
+        /// </para> 
+        /// <para>Значение, связанное с  
+        /// <paramref name="key"/> в  
+        /// <paramref name="dictionary"/> если оно существует, иначе новое значение. 
+        /// </para> 
         /// </returns>        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> valueFactory)
