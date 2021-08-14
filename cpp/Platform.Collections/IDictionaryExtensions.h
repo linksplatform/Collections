1391cb67626222cabcf59986e88795d79c2c0b39
uselessgoddess <dodickgod@gmail.com>
2021-08-14 20:48:52 +0300

    Rewrite Node to modern style

--- cpp/Platform.Collections/IDictionaryExtensions.h
+++ cpp/Platform.Collections/IDictionaryExtensions.h
@@ -1,14 +1,14 @@
 ﻿namespace Platform::Collections::Dictionaries
 {
-    template<Interfaces::IDictionary TDictionary>
-    void Add(TDictionary& dictionary, auto&& key, auto&& value)
+    //template<Interfaces::IDictionary TDictionary>
+    void Add(/*TDictionary&*/auto& dictionary, auto&& key, auto&& value)
     {
         if (dictionary.contains(key))
         {
             throw std::logic_error("Unknown exception");
         }
-        using Item = typename Interfaces::Dictionary<TDictionary>::Item;
-        dictionary.insert(Item{std::forward<decltype(key)>(key), std::forward<decltype(value)>(value)});
+        //using Item = typename Interfaces::Dictionary<TDictionary>::Item;
+        dictionary.insert({std::forward<decltype(key)>(key), std::forward<decltype(value)>(value)});
     }
 
     template<Interfaces::IDictionary TDictionary>
--- cpp/Platform.Collections/Trees/Node.h
+++ cpp/Platform.Collections/Trees/Node.h
@@ -11,7 +11,7 @@
     };
 
     template<typename T>
-    concept NotHelperType = !std::derived_from<T, HelperTypeTag>;
+    concept NotHelperType = not std::derived_from<T, HelperTypeTag>;
 
     template<typename TValue, typename...>
     class Node;
@@ -31,57 +31,69 @@
     template<typename TValue, typename TKey>
     class Node<TValue, Repeat<TKey>>
     {
-        static_assert(std::default_initializable<TValue>);
+        static_assert(std::default_initializable<TValue>,
+            "todo!");
+
+        public: using Child = Node;
+        public: using RefChild = std::reference_wrapper<Child>;
+        public: using ConstRefChild = std::reference_wrapper<const Child>;
+        public: using RefValue = std::reference_wrapper<TValue>;
 
         public: TValue Value;
-        public: using Child = Node;
 
-        private: std::unordered_map<TKey, Child*> _childNodes;
-        public: auto ChildNodes() -> auto& { return _childNodes; }
+        using dict_type = std::unordered_map<TKey, Child>;
+        private: std::unique_ptr<dict_type> _childNodes = std::make_unique<dict_type>();
+        public: auto ChildNodes() const -> auto&& { return *_childNodes; }
 
         public: explicit Node(TValue value = {}) : Value(std::move(value)) { }
 
+        public: Node(const Node& other)
+            : Value(other.Value), _childNodes(std::make_unique<dict_type>(other.ChildNodes())) { }
+
         public: auto operator[](const TKey& key) -> Child&
         {
-            if(!_childNodes.contains(key))
+            if(!ChildNodes().contains(key))
                 return AddChild(key);
 
-            return *_childNodes[key];
+            return ChildNodes()[key];
         }
 
-        public: auto ContainsChild(const std::vector<TKey>& keys) -> bool { return GetChild(keys) != nullptr; }
+        public: auto ContainsChild(const std::vector<TKey>& keys) const -> bool { return GetChild(keys).has_value(); }
 
-        public: auto GetChild(const std::vector<TKey>& keys) -> Child*
+        public: auto GetChild(const std::vector<TKey>& keys) const -> auto
         {
-            auto node = this;
+            auto* node = this;
             for (auto&& key : keys)
             {
-                node = node->ChildNodes()[key];
-                if (node == nullptr)
+                if (node->ChildNodes().contains(key))
                 {
-                    return node;
+                    node = &node->ChildNodes().at(key);
+                }
+                else
+                {
+                    return decltype(std::optional{std::ref(*node)})(std::nullopt);
                 }
             }
-            return node;
+            return std::optional{std::ref(*node)};
         }
 
-        public: auto GetChildValue(const std::vector<TKey>& keys) -> TValue*
+        public: auto GetChildValue(const std::vector<TKey>& keys) const -> std::optional<const RefValue>
         {
-            auto child = GetChild(keys);
-            return (child == nullptr) ? nullptr : &child->Value;
+            auto child = (std::optional<RefChild>)GetChild(keys);
+            return (child.has_value()) ? child.value().Value : std::nullopt;
         }
 
         public: auto AddChild(const TKey& key, const TValue& value = {}) -> Child& { return AddChild(key, Child(value)); }
 
-        public: auto AddChild(TKey key, const Child& child) -> Child&
+        public: auto AddChild(TKey key, Child child) -> Child&
         {
-            Dictionaries::Add(_childNodes, key, new Child{child});
-            return *_childNodes[key];
+            Dictionaries::Add(ChildNodes(), key, std::move(child));
+            return ChildNodes()[key];
         }
 
-        public: auto SetChild(const std::vector<TKey>& keys) -> Child& { return SetChildValue(TValue{}, keys); }
+        public: auto SetChild(const std::vector<TKey>& keys) -> Child& { return SetChildValue({}, keys); }
 
-        public: auto SetChild(TKey key) -> Child& { SetChildValue(TValue{}, key); }
+        public: auto SetChild(TKey key) -> Child& { SetChildValue({}, key); }
 
         public: auto SetChildValue(const TValue& value, const std::vector<TKey>& keys) -> Child&
         {
