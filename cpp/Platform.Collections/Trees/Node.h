6af39538acb04257f4ac7a4a6970400953c05bb2
uselessgoddess <dodickgod@gmail.com>
2021-08-15 18:14:51 +0300

    Rework model

--- cpp/Platform.Collections/Trees/Node.h
+++ cpp/Platform.Collections/Trees/Node.h
@@ -13,6 +13,12 @@
     template<typename T>
     concept NotHelperType = not std::derived_from<T, HelperTypeTag>;
 
+    template<typename Self>
+    concept ValueNode = requires(Self self)
+    {
+        self.Value;
+    };
+
     template<typename TValue, typename...>
     class Node;
 
@@ -30,24 +36,17 @@
 
     namespace Internal
     {
-        template<typename Self>
-        concept ValueNode = requires(Self self)
-        {
-            self.Value;
-        };
-
         template<typename TValue, typename TKey, typename Child>
-        struct NodeBase : public Node<TValue>
+        class NodeBase
         {
-            using base = Node<TValue>;
-            using base::base;
-
             using dict_type = std::unordered_map<TKey, Child>;
             private: std::unique_ptr<dict_type> _childNodes = std::make_unique<dict_type>();
             public: auto ChildNodes() const -> auto&& { return *_childNodes; }
 
+            public: NodeBase() = default;
+
             public: NodeBase(const NodeBase& other)
-                : base(other.Value), _childNodes(std::make_unique<dict_type>(other.ChildNodes())) { }
+                : _childNodes(std::make_unique<dict_type>(other.ChildNodes())) { }
 
             public: auto operator[](const TKey& key) -> Child&
             {
@@ -57,9 +56,9 @@
                 return ChildNodes()[key];
             }
 
-            public: auto ContainsChild(const std::vector<TKey>& keys) const -> bool { return GetChild(keys).has_value(); }
+            public: auto ContainsChild(std::ranges::range auto&& keys) const -> bool { return GetChild(std::forward<decltype(keys)>(keys)).has_value(); }
 
-            public: auto GetChild(const std::vector<TKey>& keys) const -> auto
+            public: auto GetChild(std::ranges::range auto&& keys) const -> auto
             {
                 auto* node = this;
                 using optional_type = decltype(std::optional{std::ref(*node)});
@@ -77,14 +76,16 @@
                 return std::optional{std::ref(*node)};
             }
 
-            public: auto GetChildValue(const std::vector<TKey>& keys) const -> auto
+            public: auto GetChildValue(std::ranges::range auto&& keys) const -> auto
             {
-                auto child = GetChild(keys);
+                auto child = GetChild(std::forward<decltype(keys)>(keys));
                 using optional_type = decltype(std::optional{std::ref(child.value().get().Value)});
                 return (child.has_value()) ? std::optional{std::ref(child.value().get().Value)} : optional_type{std::nullopt};
             }
 
-            public: auto AddChild(const TKey& key, const TValue& value = {}) -> Child& { return AddChild(key, Child(value)); }
+            public: auto AddChild(const TKey& key, const TValue& value) -> Child& { return AddChild(key, Child{value}); }
+
+            public: auto AddChild(const TKey& key) -> Child& { return AddChild(key, Child{}); }
 
             public: auto AddChild(TKey key, Child child) -> Child&
             {
@@ -92,11 +93,9 @@
                 return ChildNodes()[key];
             }
 
-            public: auto SetChild(const std::vector<TKey>& keys) -> Child& { return SetChildValue({}, keys); }
+            public: auto SetChild(auto&&... keys) -> Child& { return SetChildValue({}, std::forward<decltype(keys)>(keys)...); }
 
-            public: auto SetChild(TKey key) -> Child& { SetChildValue({}, key); }
-
-            public: auto SetChildValue(const TValue& value, const std::vector<TKey>& keys) -> Child&
+            public: auto SetChildValue(const TValue& value, std::ranges::range auto&& keys) -> Child&
             {
                 auto node = this;
                 for (auto&& key : keys)
@@ -106,21 +105,41 @@
                 node->Value = value;
                 return *node;
             }
-
-            public: auto SetChildValue(const TValue& value, const TKey& key) -> Child&
-            {
-                auto& child = (*this)[key];
-                child.Value = value;
-                return child;
-            }
         };
+
+        template<typename TNode, std::size_t depth>
+        consteval auto ChildHasValueHelper()
+        {
+            if constexpr (depth == 0) {
+                return ValueNode<TNode>;
+            } else {
+                return requires(TNode node) {
+                    requires ChildHasValueHelper<decltype(node.ChildNodes().at(0)), depth - 1>();
+                };
+            }
+        }
+
+        template<typename TNode, std::size_t depth>
+        consteval auto ChildHasValue()
+        {
+            return ChildHasValueHelper<TNode, depth>();
+        }
     }
 
     template<typename TValue, typename TKey>
-    class Node<TValue, Repeat<TKey>> : public Internal::NodeBase<TValue, TKey, Node<TValue, Repeat<TKey>>>
+    struct Node<TValue, Repeat<TKey>>
+        : public Internal::NodeBase<TValue, TKey, Node<TValue, Repeat<TKey>>>,
+          public Node<TValue>
     {
         using base = Internal::NodeBase<TValue, TKey, Node<TValue, Repeat<TKey>>>;
         using base::base;
+
+        using leaf_base = Node<TValue>;
+        using leaf_base::leaf_base;
+
+        Node() : base(), leaf_base() {}
+
+        Node(const Node& other) : base(other), leaf_base(other) {}
     };
 
     template<typename TValue, NotHelperType TKey, typename... Tail>
@@ -130,8 +149,43 @@
         using base = Internal::NodeBase<TValue, TKey, Child>;
         using base::base;
 
-        auto& AddChild(const TKey& key, const TValue& value = {}) requires Internal::ValueNode<Child> { return base::AddChild(key, Child{value}); };
-        auto GetChildValue(auto&&... args) requires Internal::ValueNode<Child> { return base::SetChildValue(std::forward<decltype(args)>(args)...); };
-        auto& SetChildValue(auto&&... args) requires Internal::ValueNode<Child> { return base::SetChildValue(std::forward<decltype(args)>(args)...); };
+        auto& AddChild(const TKey& key, const TValue& value) requires ValueNode<Child> { return base::AddChild(key, value); };
+
+        auto& GetChild(auto&&... keys) const
+        {
+            std::tuple tuple_keys = { std::forward<decltype(keys)>(keys)... };
+            return _GetChild(std::move(tuple_keys), *this);
+        }
+
+        public: auto GetChildValue(auto&&... keys) requires (Internal::ChildHasValue<Node, sizeof...(keys)>())
+        {
+            return GetChild(std::forward<decltype(keys)>(keys)..., *this).Value;
+        };
+
+        auto& SetChildValue(auto&& value, auto&&... keys) requires (Internal::ChildHasValue<Node, sizeof...(keys)>())
+        {
+            auto& child = GetChild(std::forward<decltype(keys)>(keys)..., *this);
+            child.Value = value;
+            return child;
+        }
+
+        auto& SetChildValue(auto&&... keys) requires (Internal::ChildHasValue<Node, sizeof...(keys)>())
+        {
+            SetChildValue(TValue{}, std::forward<decltype(keys)>(keys)...);
+        }
+
+    private:
+        template<std::size_t depth = 0>
+        auto& _GetChild(auto tuple_keys, auto& root) const
+        {
+            if constexpr (std::tuple_size_v<decltype(tuple_keys)> - 2 == depth)
+            {
+                return root.ChildNodes().at(std::get<depth>(tuple_keys));
+            }
+            else
+            {
+                return _GetChild<depth + 1>(std::move(tuple_keys), root.ChildNodes().at(std::get<depth>(tuple_keys)));
+            }
+        }
     };
 }
