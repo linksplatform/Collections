3643cf14cbf82df06b22df1ec7f9dd0bc5c68f8d
uselessgoddess <dodickgod@gmail.com>
2021-08-16 09:39:57 +0300

    String fixes

--- csharp/Platform.Collections.Tests/WalkersTests.cs
+++ csharp/Platform.Collections.Tests/WalkersTests.cs
@@ -14,7 +14,7 @@ namespace Platform.Collections.Tests
     {
         private static readonly string elfen_lied = @"Nacht im Dorf der Wächter rief: Elfe! Ein ganz kleines Elfchen im Walde schlief wohl um die Elfe! Und meint, es rief ihm aus dem Tal bei seinem Namen die Nachtigall, oder Silpelit hätt' ihm gerufen.
 Reibt sich der Elf' die Augen aus, begibt sich vor sein Schneckenhaus und ist als wie ein trunken Mann, sein Schläflein war nicht voll getan, und humpelt also tippe tapp durch’s Haselholz in’s Tal hinab, schlupft an der Mauer hin so dicht, da sitzt der Glühwurm Licht an Licht.
-Was sind das helle Fensterlein? Da drin wird eine Hochzeit sein: die Kleinen sitzen bei’m Mahle, und treiben’s in dem Saale. Da guck' ich wohl ein wenig 'nein!\'
+Was sind das helle Fensterlein? Da drin wird eine Hochzeit sein: die Kleinen sitzen bei’m Mahle, und treiben’s in dem Saale. Da guck' ich wohl ein wenig 'nein!""
 Pfui, stößt den Kopf an harten Stein! Elfe, gelt, du hast genug? Gukuk!";
         private static readonly string _exampleText =
     @"([english version](https://github.com/Konard/LinksPlatform/wiki/About-the-beginning))
