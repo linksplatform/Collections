1e5874276a10e3a4333b260bac7bf91646422783
Melovka <shafikov.artur2016@yandex.ru>
2021-08-27 22:10:03 +0500

    New version2

--- csharp/Platform.Collections/Segments/Walkers/AllSegmentsWalkerBase[T].cs
+++ csharp/Platform.Collections/Segments/Walkers/AllSegmentsWalkerBase[T].cs
@@ -13,8 +13,8 @@ namespace Platform.Collections.Segments.Walkers
     public abstract class AllSegmentsWalkerBase<T> : AllSegmentsWalkerBase<T, Segment<T>>
     {
         /// <summary>
-        /// <para>Initializes a new instance of the <see cref="Segment"/> class, using the <paramref name="base"/> list of the segment, Offset relative to the list<paramref name="offset"/> and The segment's length <paramref name="length" />.</para>
-        /// <para>Инициализирует новый экземпляр класса <see cref="Segment"/>, используя исходный список сегмента, <paramref name="base"/> Смещение относительно списка<paramref name="offset"/> и его длинну. <paramref name="length"/> </para>
+        /// <para>Initializes a new instance of the <see cref="Segment"/> class, using the <paramref name="base"/>,<paramref name="offset"/> and <paramref name="length" />.</para>
+        /// <para>Инициализирует новый экземпляр класса <see cref="Segment"/>, используя <paramref name="base"/>,<paramref name="offset"/> и <paramref name="length"/> </para>
         /// </summary>
         /// <param name="base"><para>The reference to the original list containing the elements of this segment.</para><para>Ссылка на исходный список в котором находятся элементы этого сегмента.</para></param>
         /// <param name="offset"><para>The offset relative to the <paramref name="base"/> list from which the segment starts.</para><para>Смещение относительно списка <paramref name="base"/>, с которого начинается сегмент.</para></param>
