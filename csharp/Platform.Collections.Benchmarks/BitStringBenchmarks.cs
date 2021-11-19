cd31df8d0fc970fba6ec9e6fa67b6db5cbd5d9f3
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 14:51:13 +0600

    Remove XML Documentation for tests and benchmarks

--- csharp/Platform.Collections.Benchmarks/BitStringBenchmarks.cs
+++ csharp/Platform.Collections.Benchmarks/BitStringBenchmarks.cs
@@ -2,33 +2,15 @@ using BenchmarkDotNet.Attributes;
 
 namespace Platform.Collections.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the bit string benchmarks.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     [SimpleJob]
     [MemoryDiagnoser]
     public class BitStringBenchmarks
     {
-        /// <summary>
-        /// <para>
-        /// Gets or sets the n value.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Params(1000, 10000, 100000, 1000000, 10000000, 100000000)]
         public int N { get; set; }
         private BitString _left;
         private BitString _right;
 
-        /// <summary>
-        /// <para>
-        /// Setup this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [GlobalSetup]
         public void Setup()
         {
@@ -41,211 +23,51 @@ namespace Platform.Collections.Benchmarks
             }
         }
 
-        /// <summary>
-        /// <para>
-        /// Nots this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString Not() => new BitString(_left).Not();
 
-        /// <summary>
-        /// <para>
-        /// Vectors the not.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString VectorNot() => new BitString(_left).VectorNot();
 
-        /// <summary>
-        /// <para>
-        /// Parallels the not.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelNot() => new BitString(_left).ParallelNot();
 
-        /// <summary>
-        /// <para>
-        /// Parallels the vector not.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelVectorNot() => new BitString(_left).ParallelVectorNot();
 
-        /// <summary>
-        /// <para>
-        /// Ands this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString And() => new BitString(_left).And(_right);
 
-        /// <summary>
-        /// <para>
-        /// Vectors the and.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString VectorAnd() => new BitString(_left).VectorAnd(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the and.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelAnd() => new BitString(_left).ParallelAnd(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the vector and.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelVectorAnd() => new BitString(_left).ParallelVectorAnd(_right);
 
-        /// <summary>
-        /// <para>
-        /// Ors this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString Or() => new BitString(_left).Or(_right);
 
-        /// <summary>
-        /// <para>
-        /// Vectors the or.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString VectorOr() => new BitString(_left).VectorOr(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the or.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelOr() => new BitString(_left).ParallelOr(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the vector or.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelVectorOr() => new BitString(_left).ParallelVectorOr(_right);
 
-        /// <summary>
-        /// <para>
-        /// Xors this instance.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString Xor() => new BitString(_left).Xor(_right);
 
-        /// <summary>
-        /// <para>
-        /// Vectors the xor.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString VectorXor() => new BitString(_left).VectorXor(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the xor.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelXor() => new BitString(_left).ParallelXor(_right);
 
-        /// <summary>
-        /// <para>
-        /// Parallels the vector xor.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The bit string</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public BitString ParallelVectorXor() => new BitString(_left).ParallelVectorXor(_right);
     }
--- csharp/Platform.Collections.Benchmarks/Program.cs
+++ csharp/Platform.Collections.Benchmarks/Program.cs
@@ -2,18 +2,8 @@ using BenchmarkDotNet.Running;
 
 namespace Platform.Collections.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the program.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     static class Program
     {
-        /// <summary>
-        /// <para>Initializing the Benchmark test.</para>
-        /// <para>Инициализация Бенчмарк теста.</para>
-        /// </summary>
         static void Main() => BenchmarkRunner.Run<BitStringBenchmarks>();
     }
 }
