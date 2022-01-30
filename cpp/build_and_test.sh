632f212557c2b3b938881c9eff714b083f70a9a5
uselessgoddess <dodickgod@gmail.com>
2022-01-30 19:14:25 +0300

    Update build script

--- cpp/build_and_test.sh
+++ cpp/build_and_test.sh
@@ -20,6 +20,7 @@ conan create platform.converters/all platform.converters/0.1.0@ -pr=linksplatfor
 conan create platform.ranges/all platform.ranges/0.1.3@ -pr=linksplatform
 conan create platform.random/all platform.random/0.1.0@ -pr=linksplatform
 cd ../..
+ls
 
 cmake_flags="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DLINKS_PLATFORM_TESTS=ON"
 cmake_build_dir="build"
