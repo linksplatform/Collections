38cbad44c68e2ce37c5dee6be7275073efca8b67
uselessgoddess <dodickgod@gmail.com>
2022-01-30 19:19:57 +0300

    Use clang in build script

--- cpp/build_and_test.sh
+++ cpp/build_and_test.sh
@@ -6,9 +6,9 @@ pip install conan
 
 conan profile new linksplatform --detect
 conan profile update settings.compiler=gcc linksplatform
-conan profile update settings.compiler.version=10 linksplatform
+conan profile update settings.compiler.version=11 linksplatform
 conan profile update settings.compiler.libcxx=libstdc++11 linksplatform
-conan profile update env.CXX=g++-10 linksplatform
+conan profile update env.CXX=clang++ linksplatform
 conan profile show linksplatform
 
 git clone https://github.com/linksplatform/conan-center-index
