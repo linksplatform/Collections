2ea91478337afb6661ec82bbfc60947bade97ed1
Vladimir <58123600+Flakeed@users.noreply.github.com>
2022-02-02 20:31:34 +0200

    Update IStack.h

--- cpp/Platform.Collections/Stacks/IStack.h
+++ cpp/Platform.Collections/Stacks/IStack.h
@@ -1,15 +1,21 @@
+#pragma once
+
 namespace Platform::Collections::Stacks
 {
+    template <typename ...>
+    struct IStack;
+
     template<typename TElement>
-    class IStack
+    struct IStack<TElement>
     {
-    public:
         virtual bool empty() = 0;
 
         virtual void push(TElement item) = 0;
 
         virtual void pop() = 0;
 
-        virtual TElement top() = 0;
+        virtual TElement& top() = 0;
+
+        virtual ~IStack() = default;
     };
 }
