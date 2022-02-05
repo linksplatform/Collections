17284233e1eef3dd532c4cded6ea99d88c6550cf
Vladimir <58123600+Flakeed@users.noreply.github.com>
2022-02-05 17:41:32 +0200

    Update StackTests.cpp

--- cpp/Platform.Collections.Tests/StackTests.cpp
+++ cpp/Platform.Collections.Tests/StackTests.cpp
@@ -1,12 +1,4 @@
 namespace Platform::Collections::Tests {
-TEST(StackTests, Concept) {
-  Stacks::CStack<int> auto stack = std::stack<int>{};
-  stack.push(1);
-  ASSERT_EQ(stack.top(), 1);
-  stack.pop();
-  ASSERT_TRUE(stack.empty());
-}
-TEST(StackTests, Interface) {
   struct EmptyStack : public Stacks::IStack<int> {
     bool empty() const {}
 
@@ -17,6 +9,18 @@ TEST(StackTests, Interface) {
     int &top() {}
 
     const int &top() const {}
+   
   };
+TEST(StackTests, Concept) {
+  Stacks::CStack<int> auto stack = std::stack<int>{};
+  stack.push(1);
+  ASSERT_EQ(stack.top(), 1);
+  stack.pop();
+  ASSERT_TRUE(stack.empty());
+}
+TEST(StackTests, Interface) {
+}
+TEST(StackTests, ConceptInterface) {
+Stacks::CStack<int> auto stack = EmptyStack{};
 }
 } // namespace Platform::Collections::Tests
