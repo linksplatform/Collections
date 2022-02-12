88b721fdcd44ad61bd7d4e761509b794d546152b
Vladimir <58123600+flakeed@users.noreply.github.com>
2022-02-12 22:38:52 +0200

    Update CStack.h

--- cpp/Platform.Collections/Stacks/CStack.h
+++ cpp/Platform.Collections/Stacks/CStack.h
@@ -7,15 +7,13 @@ namespace Platform::Collections::Stacks
         requires(TSelf & self, TElement item)
     {
         { self.push(item) };
-
-        { self.pop() };
-
-        { self.top() } -> std::same_as<TElement&>;
     } &&
         requires(const TSelf& self, TElement item)
     {
         { self.empty() } -> std::same_as<bool>;
 
-        { self.top() } -> std::same_as<const TElement&>;
+        { self.pop() };
+
+        { self.top() } -> std::same_as<TElement&>;
     };
 }
