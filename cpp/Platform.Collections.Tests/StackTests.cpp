6d944f234a0f85be225a5e03de922612b17be67f
uselessgoddess <68294279+uselessgoddess@users.noreply.github.com>
2022-02-13 09:11:43 +0300

    Apply suggestions from code review

--- cpp/Platform.Collections.Tests/StackTests.cpp
+++ cpp/Platform.Collections.Tests/StackTests.cpp
@@ -2,28 +2,28 @@
 
 namespace Platform::Collections::Tests 
 {
-struct EmptyStack : public Stacks::IStack<int> 
-{
-    bool empty() const {}
+    struct EmptyStack : public Stacks::IStack<int> 
+    {
+        bool empty() const {}
 
-    void push(int item) {}
+        void push(int item) {}
 
-    void pop() {}
+        void pop() {}
 
-    int &top() {}
+        int &top() {}
 
-    const int &top() const {}
-};
+        const int &top() const {}
+    };
 
-TEST(StackTests, Concept) 
-{
-    Stacks::CStack<int> auto stack = std::stack<int>{};
-    stack.push(1);
-    ASSERT_EQ(stack.top(), 1);
-    stack.pop();
-    ASSERT_TRUE(stack.empty());
+    TEST(StackTests, Concept) 
+    {
+        Stacks::CStack<int> auto stack = std::stack<int>{};
+        stack.push(1);
+        ASSERT_EQ(stack.top(), 1);
+        stack.pop();
+        ASSERT_TRUE(stack.empty());
+    }
+
+    TEST(StackTests, Interface) {}
+    TEST(StackTests, ConceptInterface) {}
 }
-
-TEST(StackTests, Interface) {}
-TEST(StackTests, ConceptInterface) {}
-} // namespace Platform::Collections::Tests
