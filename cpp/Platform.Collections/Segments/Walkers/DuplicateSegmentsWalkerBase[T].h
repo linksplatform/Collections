da7ede7420f7e1adc5ddacbe4ec578f7f20d0182
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 14:11:56 +0600

    Revert "Add .net6 support"
    This reverts commit f610e3eb772be8abc3cf690f5bdbc77dff7ea76e.

--- cpp/Platform.Collections/Segments/Walkers/DuplicateSegmentsWalkerBase[T].h
+++
@@ -1,7 +0,0 @@
-﻿namespace Platform::Collections::Segments::Walkers
-{
-    template <typename ...> class DuplicateSegmentsWalkerBase;
-    template <typename T> class DuplicateSegmentsWalkerBase<T> : public DuplicateSegmentsWalkerBase<T, Segment<T>>
-    {
-    }
-}
